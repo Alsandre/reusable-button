c76c884a94924fa9fe7b4918dc2d29ebff29ec9c
alsandre <gabeditebuli@gmail.com>
2023-12-21 23:12:52 +0400

    feat: basic animation setup

--- cube.js
+++ cube.js
@@ -1,19 +1,9 @@
 function buttonClick(e) {
-  let btnClassList = e.target.classList;
-  let btnFrontClassList = document.getElementById("btn-front").classList;
-  let btnLeftClassList = document.getElementById("btn-left").classList;
-
-  btnClassList.contains("pushedFace")
-    ? btnClassList.remove("pushedFace")
-    : btnClassList.add("pushedFace");
-
-  btnFrontClassList.contains("pushedFront")
-    ? btnFrontClassList.remove("pushedFront")
-    : btnFrontClassList.add("pushedFront");
-
-  btnLeftClassList.contains("pushedLeft")
-    ? btnLeftClassList.remove("pushedLeft")
-    : btnLeftClassList.add("pushedLeft");
-
-    new Audio('assets/click.wav').play()
+  let button = e.target.parentElement;
+  let classList = button.classList;
+  classList.contains("click")
+    ? classList.remove("click")
+    : classList.add("click");
+  console.log(button);
+  new Audio("assets/click.wav").play();
 }
