a1bfc5b969b60dd6c5ab9ca0d9e4cdb622dd747b
alsandre <gabeditebuli@gmail.com>
2023-12-22 16:48:29 +0400

    refactor event anim logic

--- cube.js
+++ cube.js
@@ -1,9 +1,16 @@
 function buttonClick(e) {
-  let button = e.target.parentElement;
-  let classList = button.classList;
-  classList.contains("clickOdd") || classList.contains("clickEven") || classList.add("clickOdd")
-  classList.contains("clickOdd")
-    ? classList.replace("clickOdd", 'clickEven')
-    : classList.replace("clickEven", 'clickOdd');
+  let btnClassList = e.target.parentElement.classList;
+
+  if (
+    !btnClassList.contains("btn-click_initial") &&
+    !btnClassList.contains("btn-click_subsequent")
+  )
+    btnClassList.add("btn-click_initial");
+  else {
+    btnClassList.contains("btn-click_initial")
+      ? btnClassList.replace("btn-click_initial", "btn-click_subsequent")
+      : btnClassList.replace("btn-click_subsequent", "btn-click_initial");
+  }
+
   new Audio("assets/click.wav").play();
 }
