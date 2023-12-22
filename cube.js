0ee11538e770eaa31a45c093701ef32dde2704f1
alsandre <gabeditebuli@gmail.com>
2023-12-22 13:06:43 +0400

    feat: implement click animation loop

--- cube.js
+++ cube.js
@@ -1,9 +1,10 @@
 function buttonClick(e) {
   let button = e.target.parentElement;
   let classList = button.classList;
-  classList.contains("click")
-    ? classList.remove("click")
-    : classList.add("click");
+  classList.contains("clickOdd") || classList.contains("clickEven") || classList.add("clickOdd")
+  classList.contains("clickOdd")
+    ? classList.replace("clickOdd", 'clickEven')
+    : classList.replace("clickEven", 'clickOdd');
   console.log(button);
   new Audio("assets/click.wav").play();
 }
